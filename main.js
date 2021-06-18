2e970e33767923fbe5db3059f4aa4708aee9964a
Ashokshanbhag <79842743+Ashokshanbhag@users.noreply.github.com>
2021-06-18 17:42:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function preload(){
+
+
+
+}
+
+function setup(){
+
+    canvas = createCanvas(300,300,100 );
+    canvas.center()
+    video = createCapture(VIDEO);
+    video.size(400,400);
+    video.hide();
+
+}
+
+function draw(){
+    
+    image(video,0,0, 400, 400);
+
+}
